d0c949f20a139a018d2713017a0ed60442531187
Khushal Agarwal <khushal.agarwal987@gmail.com>
2021-02-04 00:13:36 +0530

    user create enhancement

--- controller/user.js
+++ controller/user.js
@@ -35,17 +35,40 @@ exports.getSpecificUser = (req, res, next) => {
 
 exports.createUser = (req, res, next) => {
     const { name, email, password, username, hobbies, bio } = req.body;
-    const user = new User({
-        name,
-        email,
-        password,
-        username,
-        hobbies,
-        bio,
-    });
-    return user.save().then((result) => {
-        return res
-            .status(200)
-            .json({ message: "New user created", user: user });
+    if (!username || !password) {
+        const error = new Error("Username and password not entered");
+        error.status = 404;
+        throw error;
+    }
+    if (password.length < 8) {
+        const error = new Error("Password is not valid not entered");
+        error.status = 404;
+        throw error;
+    }
+    if (hobbies.length === 0) {
+        const error = new Error("User must have a hobby");
+        error.status = 404;
+        throw error;
+    }
+    User.find({ email }).then((result) => {
+        if (result) {
+            const error = new Error("User already exists for the email");
+            error.status = 404;
+            throw error;
+        } else {
+            const user = new User({
+                name,
+                email,
+                password,
+                username,
+                hobbies,
+                bio,
+            });
+            return user.save().then((result) => {
+                return res
+                    .status(200)
+                    .json({ message: "New user created", user: user });
+            });
+        }
     });
 };
